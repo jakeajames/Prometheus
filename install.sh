6cf0c6bf7cdd12d8c1d8da0da45688ba38a6e960
Jake James <jakeashacks@gmail.com>
2017-08-27 00:08:59 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,8 +6,13 @@ echo "Installing brew"
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 echo "Installing dependencies.."
-brew install libimobiledevice
-brew install ideviceinstaller
+brew uninstall ideviceinstaller
+brew uninstall libimobiledevice
+brew install --HEAD libimobiledevice
+brew link --overwrite libimobiledevice
+brew install --HEAD ideviceinstaller
+brew link --overwrite ideviceinstaller
+sudo chmod -R 777 /var/db/lockdown/
 echo "Fixing stuff..."
 echo "You may be asked for your admin password now, enter it and click return"
 sleep 1
